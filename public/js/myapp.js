61f49223206a1080ef7e49443223afce61fc3ac3
Ahmad <a7.ahmad0@gmail.com>
2020-08-16 22:34:49 +0300

    remove unnecessary console logs

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,5 +1,3 @@
-console.log("client side is active");
-
 const weatherform = document.querySelector("form");
 const search = document.querySelector("input");
 const msgOne = document.querySelector("#msgOne");
